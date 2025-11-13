a36db713645efe93852ad085f4acd7a946f81e6e
schutz-luca <lucaschutzenhofer@hotmail.com>
2025-11-13 01:47:32 -0300

    update environment file path to use the correct target for environment.ts

--- set-env.ts
+++ set-env.ts
@@ -1,6 +1,6 @@
 const { writeFileSync } = require('fs');
 
-const targetPath = './src/environments/environment.prod.ts';
+const targetPath = './src/environments/environment.ts';
 const envConfigFile = `
 export const environment = {
   production: true,
